38d6aad69a3fe953e542d9adc045e0657d7acb25
Алан Джелиев <djalan84@mail.ru>
2025-01-08 23:34:50 +0300

    Feature: multiple manguage support

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -1,5 +1,7 @@
 from .base_page import BasePage
 from .locators import BasketPageLocators
+from selenium.webdriver.common.by import By
+
 
 class BasketPage(BasePage):
     def __init__(self, *args, **kwargs):
@@ -10,5 +12,31 @@ class BasketPage(BasePage):
     
 
     def empty_basket_message_present(self):
+        languages = {
+            "ar": "سلة التسوق فارغة",
+            "ca": "La seva cistella està buida.",
+            "cs": "Váš košík je prázdný.",
+            "da": "Din indkøbskurv er tom.",
+            "de": "Ihr Warenkorb ist leer.",
+            "en": "Your basket is empty.",
+            "el": "Το καλάθι σας είναι άδειο.",
+            "es": "Tu carrito esta vacío.",
+            "fi": "Korisi on tyhjä",
+            "fr": "Votre panier est vide.",
+            "it": "Il tuo carrello è vuoto.",
+            "ko": "장바구니가 비었습니다.",
+            "nl": "Je winkelmand is leeg",
+            "pl": "Twój koszyk jest pusty.",
+            "pt": "O carrinho está vazio.",
+            "pt-br": "Sua cesta está vazia.",
+            "ro": "Cosul tau este gol.",
+            "ru": "Ваша корзина пуста",
+            "sk": "Váš košík je prázdny",
+            "uk": "Ваш кошик пустий.",
+            "zh-cn": "Your basket is empty.",
+        }
         message = self.browser.find_element(*BasketPageLocators.BASKET_MESSAGE)
-        assert 'Your basket is empty' in message.text
+        browser_language = self.browser.find_element(By.XPATH, "//html").get_attribute('lang')
+        if browser_language in languages:
+            assert languages[browser_language] in message.text
+        print('Language undetected')
